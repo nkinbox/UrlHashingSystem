8d9a1844f47b89fdc08901377ee583a5245242e8
N Kumar <nkinbox@outlook.com>
2023-05-21 19:36:27 +0530

    API to add URL

--- app/Http/Controllers/UrlRequestController.php
+++ app/Http/Controllers/UrlRequestController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UrlRequest;
 
 class UrlRequestController extends Controller
 {
-    //
+    /**
+     * Method to list Url shortening requests
+     */
+    public function index()
+    {
+        // @todo Add filters, Pagination, Sorting
+
+        $urlRequests = UrlRequest::all();
+
+        return response()->json([
+            'success' => true,
+            'data' => $urlRequests
+        ]);
+
+    }
+    /**
+     * Method to accept Url shortening request
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'long_url' => 'required|string',
+            'is_once' => 'nullable|boolean'
+        ]);
+
+        $newRequest = new UrlRequest;
+        $newRequest->long_url = $request->long_url;
+        $newRequest->is_once = $request->is_once?true:false;
+        $newRequest->is_active = false;
+        $newRequest->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Url shortening in process.',
+            'resource' => $newRequest->toArray()
+        ]);
+    }
+    
 }
--- routes/apiV1.php
+++ routes/apiV1.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrlRequestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// @todo add Auth middleware
+Route::prefix('admin')->group(function() {
+    Route::get('/urls', [UrlRequestController::class, 'index'])->name('urls.index');
+    Route::post('/urls', [UrlRequestController::class, 'store'])->name('urls.create');
 });
